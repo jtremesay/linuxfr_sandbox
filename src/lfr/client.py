661b4692d97084306151c58b9177a9fb87803e23
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-03-12 22:04:01 +0100

    feat: define BASE_URL in settings and use it in client configuration

--- src/lfr/client.py
+++ src/lfr/client.py
@@ -1,7 +1,9 @@
 from hishel.httpx import SyncCacheClient as Client
 
+from lfr.settings import BASE_URL
+
 
 def get_client() -> Client:
     return Client(
-        base_url="https://linuxfr.org",
+        base_url=BASE_URL,
     )
--- src/lfr/settings.py
+++ src/lfr/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
 
+BASE_URL = "https://linuxfr.org"
+
 BASE_DIR = Path()
 HTML_DIR = BASE_DIR / "html"
