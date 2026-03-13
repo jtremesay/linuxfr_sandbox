e9544363fa22f3cd4ce5791bac6f87ae45012176
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-03-13 10:30:37 +0100

    add pgvector

--- src/linuxfr/settings.py
+++ src/linuxfr/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path().resolve()
 
@@ -73,10 +75,9 @@ WSGI_APPLICATION = "linuxfr.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://linuxfr:linuxfr@localhost:5432/linuxfr"
+    )
 }
 
 
