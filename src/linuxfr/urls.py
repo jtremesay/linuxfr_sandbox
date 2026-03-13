d185df55308f9c164d879b8615cda48addee11c2
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-03-13 15:36:49 +0100

    minimal stat pahe

--- src/linuxfr/urls.py
+++ src/linuxfr/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from linuxfr import views
+
 urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
     path("admin/", admin.site.urls),
 ]
--- src/linuxfr/views.py
+++ src/linuxfr/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.views.generic import TemplateView
 
-# Create your views here.
+from linuxfr.models import Page, Profile, SitemapEntry
+
+
+class IndexView(TemplateView):
+    template_name = "linuxfr/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sitemap_entries_count"] = SitemapEntry.objects.count()
+        context["pages_count"] = Page.objects.count()
+        context["missing_pages_count"] = SitemapEntry.objects.filter(
+            page__isnull=True
+        ).count()
+        context["out_of_date_pages_count"] = Page.objects.filter(
+            fetched_at__lt=F("sitemap_entry__last_modified")
+        ).count()
+        context["profiles_count"] = Profile.objects.count()
+
+        return context
